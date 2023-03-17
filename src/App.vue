d90e6f0ea8cde1a0f1307c5db75fc9c9c63dc0ef
li88qq <573258011@qq.com>
2023-03-17 23:40:10 +0800

    增加eslint等,移除测试

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,8 @@
 </template>
 
 <script setup lang="ts">
-var aa = 12
-const aab = "123"
+
 </script>
 <style lang="less" scoped>
-@ab:123
-.aaa{
-  &-a{
-    background-color: @abd;
-  }
-  aaa{
-    123
-  }
-}
+
 </style>
